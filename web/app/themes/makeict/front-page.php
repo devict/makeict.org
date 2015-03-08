660c37ccb109d05075edd9f643b8f69fcca0f54c
Seth Etter <sethetter@gmail.com>
2015-03-08 13:18:46 -0500

    Make home page sections editable from interface!

--- web/app/themes/makeict/front-page.php
+++ web/app/themes/makeict/front-page.php
@@ -1,38 +1,38 @@
 <?php get_header(); ?>
 
-<!-- Main jumbotron for a primary marketing message or call to action -->
-<div class="jumbotron feature">
-  <div class="container">
-    <h1>We are Wichita's MakerSpace</h1>
-    <p class="lead">Our mission is simple: We innovate, learn, and build community at the intersection of art, technology, science, and culture.</p>
-    <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
-  </div>
-</div>
+<!-- THE LOOOOOOOOOOOP -->
+<?php if (have_posts()) : ?>
+<?php while (have_posts()) : the_post(); ?>
 
-<div class="container">
-  <!-- Example row of columns -->
-  <div class="row">
-    <div class="col-md-4">
-      <h2>What's a MakerSpace?</h2>
-      <p>A MakerSpace is a place where people with common interests can meet, socialise and/or collaborate.</p>
-      <p>MakerSpaces can be viewed as open community labs incorporating elements of machine shops, workshops and/or studios where makers can come together to share resources and knowledge to build and make things.</p>
-      <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-    </div>
-    <div class="col-md-4">
-      <h2>Where are we located?</h2>
-      <p>
-        <iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d789.3777667150504!2d-97.35123800000004!3d37.684194999999946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzfCsDQxJzAzLjEiTiA5N8KwMjEnMDQuNSJX!5e0!3m2!1sen!2sus!4v1417504671477" frameborder="0"></iframe>
-        <br>
-        <a href="https://www.google.com/maps/place/37%C2%B041'03.1%22N+97%C2%B021'04.5%22W/@37.684195,-97.351238,19z/data=!3m1!4b1!4m2!3m1!1s0x0:0x0">914 W Douglas St.</a>
-      </p>
-   </div>
-    <div class="col-md-4">
-      <h2>How can I get involved?</h2>
-      <p>MakeICT is always excited to see new faces. You can become a member by visiting <a href="http://makeict.wildapricot.org/" title="Become a MakeICT member">this page</a>. Becoming a member comes with great benefits. Read more about it <a href="/join" title="Member Info">here</a>.</p>
-      <p>It takes a lot to keep our space running, and we appreciate all the help we can get. Another great way to help is the <a href="http://makeict.wildapricot.org/donate" title="Donate to MakeICT">make a donation</a></p>
-      <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+  <?php $custom_fields = get_post_custom(get_the_ID()); ?>
+
+  <!-- Main jumbotron for a primary marketing message or call to action -->
+  <div class="jumbotron feature">
+    <div class="container">
+      <?php the_content(); ?>
     </div>
   </div>
-</div>
+
+  <div class="container">
+    <!-- Example row of columns -->
+    <div class="row">
+      <div class="col-md-4">
+        <h2><?php echo $custom_fields['Column 1 Header'][0]; ?></h2>
+        <?php echo $custom_fields['Column 1 Body'][0]; ?>
+      </div>
+      <div class="col-md-4">
+        <h2><?php echo $custom_fields['Column 2 Header'][0]; ?></h2>
+        <?php echo $custom_fields['Column 2 Body'][0]; ?>
+       </div>
+      <div class="col-md-4">
+        <h2><?php echo $custom_fields['Column 3 Header'][0]; ?></h2>
+        <?php echo $custom_fields['Column 3 Body'][0]; ?>
+      </div>
+    </div>
+  </div>
+
+<?php endwhile; ?>
+<?php endif; ?>
+
 
 <?php get_footer(); ?>
