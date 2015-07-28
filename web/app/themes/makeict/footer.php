cbb4401f3323ed0bed8e1abbf60ab871dc20edf5
Seth Etter <sethetter@gmail.com>
2015-07-27 22:52:15 -0500

    Map and tweets in footer

--- web/app/themes/makeict/footer.php
+++ web/app/themes/makeict/footer.php
@@ -1,7 +1,25 @@
-    <div class="container">
+    <div class="container footer">
       <hr>
       <footer>
-        <p>&copy; MakeICT 2014</p>
+        <div class="row">
+          <div class="col-xs-6">
+            <p>
+              <!--<iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3157.434756550626!2d-97.31836854523011!3d37.68598759967942!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87bae3978b26469b%3A0x88ac54a49adf064d!2s1500+E+Douglas+Ave%2C+Wichita%2C+KS+67214!5e0!3m2!1sen!2sus!4v1428366505404" frameborder="0"></iframe>-->
+              <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3157.434756550626!2d-97.31836854523011!3d37.68598759967942!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87bae3978b26469b%3A0x88ac54a49adf064d!2s1500+E+Douglas+Ave%2C+Wichita%2C+KS+67214!5e0!3m2!1sen!2sus!4v1428366505404"
+              frameborder="0"></iframe>
+              <!--<br>-->
+              <!--<a href="https://www.google.com/maps/place/1500+E+Douglas+Ave,+Wichita,+KS+67214/">1500 E Douglas Ave</a>-->
+            </p>
+            <p>&copy; MakeICT 2014</p>
+          </div>
+          <div class="col-xs-6">
+            <a class="twitter-timeline" data-dnt="true"
+              href="https://twitter.com/Make_ICT"
+              data-widget-id="625870029108740098">Tweets by @Make_ICT</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          </div>
+        </div>
       </footer>
     </div> <!-- /container -->
 
